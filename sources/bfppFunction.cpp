8b75e7a87037851c771024f91ac12010f6b7ccc3
MagicTINTIN <valentinservieres@gmail.com>
2023-12-12 20:09:33 +0200

    add bfpp function add OB CB valid and main defs

--- sources/bfppFunction.cpp
+++ sources/bfppFunction.cpp
@@ -5,6 +5,16 @@
 #include <stdio.h>
 #include "bfppFunction.h"
 
+std::string tabGen(int numberOfTabs)
+{
+    std::string rtn = "";
+    for (size_t i = 0; i < numberOfTabs; i++)
+    {
+        rtn += TAB;
+    }
+    return rtn;
+}
+
 /* FUNCTION GENERATOR */
 
 bfppFunction::bfppFunction(std::string functionName) : name(functionName)
@@ -17,8 +27,73 @@ bfppFunction::~bfppFunction()
 
 std::string bfppFunction::toStr()
 {
-    std::string rtn = name == "main" ? "int " : "void ";
-    rtn += name + "()\n{\n";
+    std::string rtn = "";
+    if (name == "main")
+    {
+        rtn += name + "int main()\n{\n";
+        rtn += TAB + "std::vector<uchar> memory;\n";
+        rtn += TAB + "memory.emplace_back(0);\n";
+        rtn += TAB + "int cursor(0);\n";
+    }
+    else
+    {
+        rtn += "void " + name + "()\n{\n";
+    }
+
+    rtn += content;
     rtn += "}\n\n";
     return rtn;
+}
+
+void bfppFunction::addOB()
+{
+    content += "while (memory[cursor] > 0)\n{\n";
+    height++;
+}
+
+bool bfppFunction::addCB()
+{
+    if (height > 1)
+    {
+        content += "}\n";
+        height--;
+        return false;
+    }
+    return true;
+}
+
+void bfppFunction::addPlus()
+{
+    content += "\n";
+}
+
+void bfppFunction::addMinus()
+{
+    content += "\n";
+}
+
+void bfppFunction::addLeft()
+{
+    content += "\n";
+}
+
+void bfppFunction::addRight()
+{
+    content += "\n";
+}
+
+void bfppFunction::addCin()
+{
+    content += "\n";
+}
+
+void bfppFunction::addCout()
+{
+    content += "\n";
+}
+
+
+bool bfppFunction::validFinalHeight()
+{
+    return height == 1;
 }
--- sources/bfppFunction.h
+++ sources/bfppFunction.h
@@ -1,13 +1,26 @@
 #pragma once
 #include <string>
 
+const std::string TAB("    ");
+
 class bfppFunction
 {
 private:
     std::string name;
     std::string content;
+    int height=0;
 public:
     bfppFunction(std::string functionName);
     ~bfppFunction();
     std::string toStr();
+    void addOB();
+    bool addCB();
+    void addPlus();
+    void addMinus();
+    void addLeft();
+    void addRight();
+    void addCin();
+    void addCout();
+
+    bool validFinalHeight();
 };
